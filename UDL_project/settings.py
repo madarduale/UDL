ba961b09111abab96063a86accb1647c6e688fa0
madarduale <madarducaale479@gmail.com>
2024-07-17 19:25:10 +0300

    updated

--- UDL_project/settings.py
+++ UDL_project/settings.py
@@ -183,16 +183,16 @@ WSGI_APPLICATION = 'UDL_project.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': env('DB_NAME'),
-        'USER': env('DB_USER'),
-        'PASSWORD': env('DB_PSW'),
-        'HOST': env('DB_HOST'),
-        'PORT': env('DB_PORT'),
-    }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.postgresql',
+    #     'NAME': env('DB_NAME'),
+    #     'USER': env('DB_USER'),
+    #     'PASSWORD': env('DB_PSW'),
+    #     'HOST': env('DB_HOST'),
+    #     'PORT': env('DB_PORT'),
+    # }
 
-        # 'default': dj_database_url.parse(env('DATABASE_URL'))
+        'default': dj_database_url.parse(env('DATABASE_URL'))
   
 }
 
